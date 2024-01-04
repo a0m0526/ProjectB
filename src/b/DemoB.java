5f43bf2e8966aed71165e662d9243412b7874bcc
Alex <a0m0526@gmail.com>
2024-01-03 19:51:52 -0600

    Initial Commit

---
+++ src/b/DemoB.java
@@ -0,0 +1,12 @@
+package b;
+
+public class DemoB {
+	
+	public static void main(String[] args) {
+		System.out.println("Demo B");
+		
+		System.out.println("Day 1");
+		System.out.println("Day 2");
+	}
+
+}
